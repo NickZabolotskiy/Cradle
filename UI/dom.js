c1a34414f931c6c99203274dafa59d0ed13bf81d
NickZabolotskiy <Nickzabolotskiy@gmail.com>
2017-03-13 19:33:32 +0300

    add row DOM

--- UI/dom.js
+++ UI/dom.js
@@ -1,7 +1,32 @@
-var i=0;
+var domService = (function () {
 
-var isUser =true;
-var userName = "NickZabolotskiy"
+function rewriteNews(){
+
+
+ var i=0;
+    for(var j=1; j<6;j+=2){
+
+    
+    var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[1];
+
+    tempN.childNodes[3].childNodes[1].textContent = articlesService.articles[i].title;
+    tempN.childNodes[5].childNodes[1].textContent = articlesService.articles[i].summary;
+    tempN.childNodes[7].childNodes[1].childNodes[1].textContent = articlesService.articles[i].author;
+    tempN.childNodes[7].childNodes[5].childNodes[1].textContent = articlesService.articles[i].createdAt.toDateString();
+    i++;
+
+    var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[3];
+
+    tempN.childNodes[3].childNodes[1].textContent = articlesService.articles[i].title;
+    tempN.childNodes[5].childNodes[1].textContent = articlesService.articles[i].summary;
+    tempN.childNodes[7].childNodes[1].childNodes[1].textContent = articlesService.articles[i].author;
+    tempN.childNodes[7].childNodes[5].childNodes[1].textContent = articlesService.articles[i].createdAt.toDateString();
+    i++;
+}
+}
+
+
+var isUser =false;
 
 function hub(){
     if(isUser == true){
@@ -11,7 +36,9 @@ function hub(){
 return false;
 }
 
-if(isUser == false){
+function logIn(userName){
+    if(userName == null){
+        isUser = false;
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[1].innerText = null;
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[5].childNodes[1].innerText = null;
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[7].childNodes[1].href = "https://vk.com/kristi_lavka";
@@ -24,7 +51,7 @@ if(isUser == false){
     for(var j=1; j<6;j+=2){
         var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[1];
 
-        console.log(tempN.childNodes[1].childNodes[3]);
+        
 
         tempN.childNodes[1].childNodes[3].style.opacity=0;
         tempN.childNodes[1].childNodes[3].style.cursor = "default";
@@ -42,6 +69,7 @@ if(isUser == false){
     }
 
 }else{
+     isUser = true;
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[1].innerText = "для";
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[5].childNodes[1].innerText = userName;
     document.body.childNodes[1].childNodes[1].childNodes[1].childNodes[7].childNodes[1].href = "https://vk.com/nik_zabolotskiy";
@@ -54,7 +82,7 @@ if(isUser == false){
     for(var j=1; j<6;j+=2){
         var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[1];
 
-        console.log(tempN.childNodes[1].childNodes[3]);
+
 
         tempN.childNodes[1].childNodes[3].style.opacity=0.4;
         tempN.childNodes[1].childNodes[3].style.cursor = "pointer";
@@ -71,39 +99,62 @@ if(isUser == false){
 
     }
 }
-
-
-
-for(var j=1; j<6;j+=2){
-
-
-    articlesService.editArticle(2,
-        {id:'34',title:'titleTemp',summary:'someSummary',createdAt: new Date('1991-08-15T23:12:44'),author:'ItsIam',content:'SomeContent'});
-
-    articlesService.removeArticle(3);
-
-    console.log( articlesService.articles)
-
-    articlesService.addArticle(
-    {id:'24',title:'NewTemp',summary:'NewSummary',createdAt: new Date('1991-08-15T23:12:44'),author:'ItsIam',content:'NewContent'});
-
-
-
-
-    var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[1];
-
-    tempN.childNodes[3].childNodes[1].textContent = articlesService.articles[i].title;
-    tempN.childNodes[5].childNodes[1].textContent = articlesService.articles[i].summary;
-    tempN.childNodes[7].childNodes[1].childNodes[1].textContent = articlesService.articles[i].author;
-    tempN.childNodes[7].childNodes[5].childNodes[1].textContent = articlesService.articles[i].createdAt.toDateString();
-    i++;
-
-    var tempN = document.body.childNodes[3].childNodes[1].childNodes[j].childNodes[3];
-
-    tempN.childNodes[3].childNodes[1].textContent = articlesService.articles[i].title;
-    tempN.childNodes[5].childNodes[1].textContent = articlesService.articles[i].summary;
-    tempN.childNodes[7].childNodes[1].childNodes[1].textContent = articlesService.articles[i].author;
-    tempN.childNodes[7].childNodes[5].childNodes[1].textContent = articlesService.articles[i].createdAt.toDateString();
-    i++;
-
+rewriteNews();
 }
+
+logIn(null);
+rewriteNews();
+
+
+function editNode(id1, title1, summary1, content1){
+     articlesService.editArticle(id1,
+        {id:'34',title: title1,summary: summary1, createdAt: new Date('1991-08-15T23:12:44'),
+        author:'ItsIam',content: content1});
+     rewriteNews();
+}
+   
+function removeNode(id1){
+    articlesService.removeArticle(id1);
+    rewriteNews();
+}
+
+function addNode(id1, title1, summary1, date1, author1, content1){
+    articlesService.addArticle(
+    {id:id1 ,title:title1 ,summary:summary1 ,createdAt: new Date(date1),
+    author:author1,content:content1});
+    rewriteNews();
+}
+
+
+  var names=[];
+       for(var i=0; i<articlesService.articles.length; i++){
+            if(!names.find(function (temp){
+                return temp == articlesService.articles[i].author
+            })) 
+             names.push(articlesService.articles[i].author);
+       }
+
+       for(var i=0; i<names.length; i++){
+        option = document.createElement('option'); 
+        option.className = "menu"; 
+        option.innerHTML = names[i]; 
+        document.body.childNodes[3].childNodes[5].childNodes[3].childNodes[9].appendChild(option); 
+
+       }
+
+       return{
+            rewriteNews: rewriteNews,
+            editNode: editNode,
+            addNode: addNode,
+            removeNode: removeNode,
+            logIn: logIn
+       }
+
+}());
+
+function rewriteNewsGlobal(){ domService.rewriteNews();}
+function addNodeGlobal(id1, title1, summary1, date1, author1, content1){
+domService.addNode(id1, title1, summary1, date1, author1, content1) }
+function removeNodeGlobal(id1){ domService.removeNode(id1) }
+function editNodeGlobal(id1, title1, summary1, content1){ domService.editNode(id1, title1, summary1, content1) }
+function logInGlobal(name){domService.logIn(name)}
