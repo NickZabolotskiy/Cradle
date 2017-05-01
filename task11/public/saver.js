dc462a7bbcb9e0f136b525ca3546eb15b35f6298
NickZabolotskiy <Nickzabolotskiy@gmail.com>
2017-05-02 01:34:57 +0300

    passport-autorisation

---
+++ task11/public/saver.js
@@ -0,0 +1,12 @@
+
+// window.onbeforeunload = function (){
+
+// }
+
+// window.addEventListener('beforeunload', function () {
+//
+//     var xhrt = new XMLHttpRequest();
+//     xhrt.open('POST', 'loadBase', false);
+//     xhrt.setRequestHeader('Content-Type', 'application/json');
+//     xhrt.send(JSON.stringify(articlesService.articles()));
+// });
